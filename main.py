2775e487702ad4f42a63d52cb6c03b2d20b80665
Ezequiel Abregu <ezequielabregu@Ezequiels-MacBook-Air.local>
2022-06-08 10:49:52 -0300

    ADD Audios

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ gold = (212, 175, 55)
 blue = (0, 255, 255)
 tickness1 = 5
 
-
 screen = pygame.display.set_mode([WITH, HEIGHT])                        #create screen
 pygame.display.set_caption('Beat Maker')
 label_font = pygame.font.Font('Roboto-Bold.ttf', 32)
@@ -30,6 +29,31 @@ playing = True
 active_length = 0
 active_beat = 1
 beat_changed = True
+
+# --- LOAD IN SOUNDS ---
+hi_hat = mixer.Sound('sounds/hi hat.wav')
+snare = mixer.Sound('sounds/snare.wav')
+kick = mixer.Sound('sounds/kick.wav')
+crash = mixer.Sound('sounds/crash.wav')
+clap = mixer.Sound('sounds/clap.wav')
+tom = mixer.Sound('sounds/tom.wav')
+
+def play_notes():
+    for i in range(len(clicked)):
+        if clicked[i][active_beat] == 1:
+            if i == 0:
+                hi_hat.play()
+            if i == 1:
+                snare.play()
+            if i == 2:
+                kick.play()
+            if i == 3:
+                crash.play()
+            if i == 4:
+                clap.play()
+            if i == 5:
+                tom.play()
+
 
 def draw_grid(clicks, beat):
     left_box = pygame.draw.rect(screen, gray, [0, 0, 200, HEIGHT], tickness1)           # left box-Arguments (x, y, with, height, tick)
@@ -73,11 +97,14 @@ def draw_grid(clicks, beat):
 
 
 run = True                                                                  # start the game
-
 while run:
     timer.tick(fps)
     screen.fill(black)                                                          # meanwhile the game is ON, fill the screen
     boxes = draw_grid(clicked, active_beat)
+    if beat_changed:
+        play_notes()
+        beat_changed = False
+        print(play_notes())
 
     for event in pygame.event.get():                                            # check if someone is pressing a key, mouse, etc (every event)
         if event.type == pygame.QUIT:                                           # if some quit, stop the game
@@ -88,7 +115,7 @@ while run:
                     coords = boxes[i][1]                                        # get coordenates (not pixels, instead boxes cells) on every click
                     clicked[coords[1]][coords[0]] *= -1
 
-    beat_length = (fps * 60) // bpm                                                   # 3600 = fps * 60 sec
+    beat_length = 3600 // bpm                                                   # 3600 = fps * 60 sec
 
     if playing:                                                                 # THIS SECTION IS A BEAT COUNTER
         if active_length < beat_length:                                         # if active length is less than beat_length...
